4ec410539fe0668612b86a128bc1cc0308c5dc09
EdwardO <edoubrayrie@gmail.com>
2021-12-21 03:06:32 +0100

    mbt_util refactor to unify DB input

--- development/src/mbt_util.py
+++ development/src/mbt_util.py
@@ -1,8 +1,9 @@
+import contextlib
 from decimal import Decimal
 import os
 import shutil
 import sqlite3
-from typing import List, Tuple
+from typing import List, Tuple, Union
 from unittest import TestCase
 
 try:
@@ -12,17 +13,54 @@ except ImportError:
     from unittest.mock import MagicMock as  T
 
 
-def get_bounds(db, dbn:str='main') -> 'tuple[Decimal, ...]': # -> w, s, e, n
-    dbc = db if isinstance(db, sqlite3.Cursor) else sqlite3.connect(db).cursor()
-    bstr, = dbc.execute(f"SELECT value FROM {dbn}.metadata WHERE name = 'bounds'").fetchone()
-    return parse_bounds(bstr)
+DB = Union[str, bytes, os.PathLike, sqlite3.Connection, sqlite3.Cursor]  # : 'TypeAlias'
+
+
+@contextlib.contextmanager
+def cursor(sqlite_or_path: DB):
+    """Gracefully handle opening/closing db if necessary."""
+    owndb = isinstance(sqlite_or_path, Union[str, bytes, os.PathLike])
+    db = sqlite3.connect(sqlite_or_path) if owndb else sqlite_or_path
+    owncur = isinstance(db, sqlite3.Connection)
+    dbc = db.cursor() if owncur else db
+    try:
+        yield dbc
+    finally:
+        if owndb and owncur:
+            db.commit()
+            db.close()
+        if owncur and not owndb:
+            dbc.close()
+
+
+# @contextlib.contextmanager
+# def cursor(sqlite_or_path: DB):
+#     """Gracefully handle opening/closing db if necessary."""
+#     owncur = not isinstance(sqlite_or_path, sqlite3.Cursor)
+#     owndb = owncur or not isinstance(sqlite_or_path, sqlite3.Connection)
+#     db = sqlite3.connect(sqlite_or_path) if owncur or owndb else sqlite_or_path
+#     dbc = db.cursor() if owncur else sqlite_or_path
+#     try:
+#         yield dbc
+#     finally:
+#         if owncur:
+#             dbc.connection.commit()
+#             dbc.close()
+#             dbc.connection.close()
+
+
+def get_bounds(sqlite_or_path, dbn:str='main') -> 'tuple[Decimal, ...]': # -> w, s, e, n
+    with cursor(sqlite_or_path) as dbc:
+        bstr, = dbc.execute(f"SELECT value FROM {dbn}.metadata WHERE name = 'bounds'").fetchone()
+        return parse_bounds(bstr)
 
 def parse_bounds(bstr):
     return tuple(map(Decimal, bstr.split(',')))
 
-def set_bounds(w, s, e, n, dbc, db:str='main'):
-    return dbc.execute(f"UPDATE {db}.metadata SET value = '{w},{s},{e},{n}' "
-                        "WHERE name = 'bounds'").fetchone()
+def set_bounds(sqlite_or_path: DB, w, s, e, n, db:str='main'):
+    with cursor(sqlite_or_path) as dbc:
+        return dbc.execute(f"UPDATE {db}.metadata SET value = '{w:.5f},{s:.5f},{e:.5f},{n:.5f}' "
+                            "WHERE name = 'bounds'").fetchone()
 
 
 def merge_bounds(w1, s1, e1, n1, w2, s2, e2, n2):
@@ -73,7 +111,7 @@ def mbt_merge(source, *more_sources:str, dest:str, name='', description='', log=
             desc += f"* {smeta['name']} : {smeta['description']}\n"
             sbounds = parse_bounds(smeta['bounds'])
             bounds = merge_bounds(*bounds, *sbounds)
-            set_bounds(*bounds, dbc)
+            set_bounds(dbc, *bounds)
             # >> Detach to make room for next source
             dbc.execute(f'DETACH source;')
             log('>>', dest[:-8], ':', mbt_info(dbc))
@@ -86,14 +124,13 @@ def mbt_merge(source, *more_sources:str, dest:str, name='', description='', log=
         db.commit()
         db.close()
 
-def mbt_info(mbt_or_cur):
-    c = mbt_or_cur if isinstance(mbt_or_cur, sqlite3.Cursor)\
-        else sqlite3.connect(mbt_or_cur).cursor()
-    res = c.execute("SELECT 'zoom =', MIN(zoom_level), MAX(zoom_level), '; n =', COUNT(*) "
-                    "FROM tiles ;").fetchall()\
-        + c.execute("SELECT ';', name, '=', value FROM metadata "
-                    "WHERE name IN ('format', 'bounds');").fetchall()
-    return ' '.join(' '.join(map(str, row)) for row in res)
+def mbt_info(mbt_or_cur: DB):
+    with cursor(mbt_or_cur) as c:
+        res = c.execute("SELECT 'zoom =', MIN(zoom_level), MAX(zoom_level), '; n =', COUNT(*) "
+                        "FROM tiles ;").fetchall()\
+            + c.execute("SELECT ';', name, '=', value FROM metadata "
+                        "WHERE name IN ('format', 'bounds');").fetchall()
+        return ' '.join(' '.join(map(str, row)) for row in res)
 
 
 # check that bounds merge work correctly
@@ -108,47 +145,54 @@ class TestMbt(TestCase):
                     (Decimal('6.768'), Decimal('44.088'), Decimal('7.734'), Decimal('46.012')))
 
 
-def create_mbt(dbc: sqlite3.Cursor, dbn:str='main'):
-    script = f'''
+def create_mbt(dbc: DB, dbn:str='main'):
+    with cursor(dbc) as dbc:
+        script = f'''
 
-        CREATE TABLE IF NOT EXISTS {dbn}.tiles (
-            zoom_level INTEGER NOT NULL,
-            tile_column INTEGER NOT NULL,
-            tile_row INTEGER NOT NULL,
-            tile_data BLOB NOT NULL,
-            UNIQUE (zoom_level, tile_column, tile_row)
-        );
+            CREATE TABLE IF NOT EXISTS {dbn}.tiles (
+                zoom_level INTEGER NOT NULL,
+                tile_column INTEGER NOT NULL,
+                tile_row INTEGER NOT NULL,
+                tile_data BLOB NOT NULL,
+                UNIQUE (zoom_level, tile_column, tile_row)
+            );
 
-        CREATE TABLE IF NOT EXISTS {dbn}.metadata (
-            name TEXT,
-            value TEXT
-        );
+            CREATE TABLE IF NOT EXISTS {dbn}.metadata (
+                name TEXT,
+                value TEXT
+            );
 
-        CREATE UNIQUE INDEX IF NOT EXISTS {dbn}.zxy ON tiles (zoom_level, tile_column, tile_row);
-        CREATE UNIQUE INDEX IF NOT EXISTS {dbn}.meta ON metadata (name);
+            CREATE UNIQUE INDEX IF NOT EXISTS {dbn}.zxy ON tiles (zoom_level, tile_column, tile_row);
+            CREATE UNIQUE INDEX IF NOT EXISTS {dbn}.meta ON metadata (name);
 
-    '''
-    dbc.executescript(script)
-
-# def copy_meta(dbcsrc, dbcdest):
+        '''
+        dbc.executescript(script)
 
 
-def create_mbt_meta(dbc: sqlite3.Cursor, name, desc, bounds:tuple, format, overwrite=True):
-    sbounds = ','.join(map(str, bounds))
-    on_conflict = 'DO UPDATE SET value=excluded.value' if overwrite else 'DO NOTHING'
-    dbc.executescript(f'''
-        INSERT INTO metadata (name, value) VALUES
-                    ("name", "{name}"),
-                    ("description", "{desc}"),
-                    ("bounds", "{sbounds}"),
-                    ("format", "{format}")
-                    ("type", "baselayer")
-                    ON CONFLICT (name)
-                    {on_conflict};
-    ''')
+def create_mbt_meta(sqlite_or_path: DB, name, desc, bounds:tuple, format, overwrite=True):
+    update_mbt_meta(sqlite_or_path, name=name, desc=desc, bounds=bounds, format=format, overwrite=overwrite)
+
+
+def update_mbt_meta(sqlite_or_path, name=None, desc=None, bounds:tuple=None, format=None,
+                    minzoom=None, maxzoom=None, overwrite=True):
+    with cursor(sqlite_or_path) as dbc:
+        dbc.execute('CREATE UNIQUE INDEX IF NOT EXISTS meta ON metadata (name)')
+        meta = {"type": "baselayer"}
+        if bounds:
+            meta['bounds'] = ','.join(map(str, bounds))
+        args = {'name': name, 'description': desc, 'format': format,
+                'minzoom': minzoom, 'maxzoom': maxzoom}
+        for k, v in args.items():
+            if v is not None:
+                meta[k] = v
+        on_conflict = 'DO UPDATE SET value=excluded.value' if overwrite else 'DO NOTHING'
+        dbc.executemany(
+            f'INSERT INTO metadata (name, value) VALUES (?, ?) ON CONFLICT (name) {on_conflict}',
+            meta.items())
     # `format` is mandatory in 1.1+
     # `bounds` is optional
-    # `description` becomes optional in
+    # `description` becomes optional later
+
 
 def insert_tiles(dbc, rows: 'List[Tuple[int, int, int, bytes]]', dbname='main'):
     """ rows: list of (z, x, y, im)"""
@@ -178,81 +222,58 @@ def remove_tiles(dbc, zxys: 'List[Tuple[int, int, int]]'):
 
 
 
-def num2tile_impl(dbc:'sqlite3.Cursor', z, x, y, flip_y, what='tile_data', dbname='main'):
-    if flip_y:
-        y = (1 << z) - y - 1
-    row = dbc.execute(f'SELECT {what} FROM {dbname}.tiles '
-                'WHERE zoom_level=? AND tile_column=? AND tile_row=?',
-                [z, x, y]).fetchone()
-    return row[0] if row else None
-
-
-def num2tile(dbc, z, x, y, flip_y=False):
+def num2tile(sqlite_or_path: DB, z:int, x:int, y:int, flip_y:bool, what='tile_data', dbname='main'):
     # `flip_y`: MBTiles usesSW origing whereas OSM, Swisstop etc use NW origin
     # https://gis.stackexchange.com/questions/116288/mbtiles-and-slippymap-tilenames
-    own = isinstance(dbc, str)
-    if own:
-        path = dbc
-        assert os.path.exists(path)
-        dbc = sqlite3.connect(path).cursor()
-    try:
-        im = num2tile_impl(dbc, z, x, y, flip_y)
-        return im
-    finally:
-        if own:
-            dbc.close()
-            dbc.connection.close()
+    with cursor(sqlite_or_path) as dbc:
+        if flip_y:
+            y = (1 << z) - y - 1
+        row = dbc.execute(f'SELECT {what} FROM {dbname}.tiles '
+                    'WHERE zoom_level=? AND tile_column=? AND tile_row=?',
+                    [z, x, y]).fetchone()
+        return row[0] if row else None
+
 
 def xyz2tile(dbc, t: 'T.Tile'):
     # Mercantile uses TXYZ but MBTiles use TMS -> flip
     return num2tile(dbc, t.z, t.x, t.y, flip_y=True)
-
-# def num2tile(dbc:'sqlite3.Cursor', z, x, y, flip_y=False):
-    # own = isinstance(dbc, str)
-    # if own:
-    #     path = dbc
-    #     assert os.path.exists(path)
-    #     db = sqlite3.connect(path)
-    #     dbc = db.cursor()
-    # if flip_y:
-    #     y = (1 << z) -y -1
-    # row = dbc.execute('SELECT tile_data FROM tiles '
-    #             'WHERE zoom_level=? AND tile_column=? AND tile_row=?',
-    #             [z, x, y]).fetchone()
-    # im = row[0] if row else None
-    # if own:
-    #     dbc.close()
-    #     db.close()
-    # return im
 
 
 def lnglat2tile(dbc:'sqlite3.Cursor|str', z, *, lng, lat):
     x, y, z = T.tile(lng, lat, z)
     return num2tile(dbc, z, x, y, flip_y=True)
 
-def get_all_tiles(db: sqlite3.Connection, q='', arraysize=1000): # <- ~30MB RAM
-    dbc = db.cursor()
-    try:
+
+def get_all_tiles(sqlite_or_path: DB, q='', arraysize=1000): # <- ~30MB RAM
+    with cursor(sqlite_or_path) as dbc:
         dbc.execute('SELECT zoom_level, tile_column, tile_row, tile_data FROM tiles ' + q)
         while rows:= dbc.fetchmany(arraysize):
             for row in rows:
                 yield row
-    finally:
-        dbc.close()
+
+
+def get_all_coords (sqlite_or_path: DB, q='', arraysize=1000): # <- ~30MB RAM
+    with cursor(sqlite_or_path) as dbc:
+        dbc.execute('SELECT zoom_level, tile_column, tile_row FROM tiles ' + q)
+        while rows:= dbc.fetchmany(arraysize):
+            for row in rows:
+                yield row
 
 
 def tile_count(dbc: sqlite3.Cursor):
     return int(dbc.execute('SELECT COUNT(*) FROM tiles').fetchone()[0])
 
+
 class Tileset:
     @classmethod
-    def from_db(cls, dbc: sqlite3.Cursor, dbname='main'):
+    def from_db(cls, sqlite_or_path: DB, dbname='main'):
         self = cls()
-        dbc.execute(f'SELECT zoom_level, tile_column, tile_row FROM {dbname}.tiles ')
-        while rows:= dbc.fetchmany(10000):
-            for row in rows:
-                self.s.add(self.zxy_to_i(*row))
-        return self
+        with cursor(sqlite_or_path) as dbc:
+            dbc.execute(f'SELECT zoom_level, tile_column, tile_row FROM {dbname}.tiles ')
+            while rows:= dbc.fetchmany(10000):
+                for row in rows:
+                    self.s.add(self.zxy_to_i(*row))
+            return self
 
     def __init__(self) -> None:
         self.s = set()
@@ -279,24 +300,3 @@ class Tileset:
         self.s.add(self.zxy_to_i(z, x, y))
     def has_tile(self, z, x, y):
         return (z, x, y) in self.s
-
-
-def get_tileset(db: sqlite3.Connection, q='', arraysize=1000): # <- ~30MB RAM
-    dbc = db.cursor()
-    try:
-        dbc.execute('SELECT zoom_level, tile_column, tile_row FROM tiles ' + q)
-        while rows:= dbc.fetchmany(arraysize):
-            for row in rows:
-                yield row
-    finally:
-        dbc.close()
-
-def get_all_coords(db: sqlite3.Connection, q='', arraysize=1000): # <- ~30MB RAM
-    dbc = db.cursor()
-    try:
-        dbc.execute('SELECT zoom_level, tile_column, tile_row FROM tiles ' + q)
-        while rows:= dbc.fetchmany(arraysize):
-            for row in rows:
-                yield row
-    finally:
-        dbc.close()
