c464b2242d4b28e09e4ecc5255f14e8db6c3c763
EdwardO <edoubrayrie@gmail.com>
2026-03-18 00:14:48 +0100

    gdal_slope_util: overviews with q3 downsampling, white_z, warp_slope, race condition fix
    - Rename make_overviews → slope_mbt_5_zooms to clarify purpose
    - Use z12 q3 downsample as base for z13/z14, upsampled to target zoom
    - Update color ramp references to eslo13cnear
    - Add add_white_z: insert transparent border tiles at low zoom
    - Add get_bounds: return WGS84 bbox via rasterio
    - Add hex-color validation in slope_mbt to catch gdaldem errors early
    - Update all callers (Central-Alps-merge, Western-Alps, sample_tests)
    - Clean up intermediate files after processing
    - Change default options for gdaldem slope to avoid race conditions

--- development/src/gdal_slope_util.py
+++ development/src/gdal_slope_util.py
@@ -4,6 +4,7 @@ from posixpath import realpath
 import re
 from subprocess import check_call, check_output, CalledProcessError
 from time import time
+import rasterio
 
 try:
     # like os.system but with live output
@@ -16,7 +17,7 @@ except ImportError:
     def check_run(cmd):
         return check_call(cmd, shell=True)
 
-from .mbt_util import mbt_merge
+from .mbt_util import mbt_merge, LLBb
 from .bbox import BBox
 
 resolutions = [
@@ -32,9 +33,10 @@ CMAPDIR_OLD = '~/code/eddy-geek/TIL/geo/data'
 CMAPDIR = os.path.realpath(os.path.realpath(__file__) + '/../../data')
 
 # ZSTD_LEVEL=3 bring an additional 15-20% at +60% processing cost, compared to L=1
-ZSTD_OPT='-co COMPRESS=ZSTD -co PREDICTOR=2 -co ZSTD_LEVEL=3 '
+ZSTD_OPT='-co COMPRESS=ZSTD -co ZSTD_LEVEL=3 '
 TILE_OPT='-co TILED=YES -co blockXsize=1024 -co blockYsize=1024 '
 XTIFF_OPT='-co BIGTIFF=YES -co SPARSE_OK=TRUE -co NUM_THREADS=ALL_CPUS '
+GDALDEM_CFG='--config GDAL_CACHEMAX 2048 --config GDAL_ENABLE_READ_WRITE_MUTEX NO '  # workaround gdaldem + NUM_THREADS deadlock
 WARP_PARAL_OPT='-multi -wo NUM_THREADS=ALL_CPUS ' # <- || compression, warp and compute
 DFLT_OPT = ZSTD_OPT + TILE_OPT + XTIFF_OPT
 DFLT_WARP_OPT = ZSTD_OPT + TILE_OPT + XTIFF_OPT + WARP_PARAL_OPT + '-overwrite '
@@ -52,23 +54,23 @@ def gdalwarp(src:str, dest:str, z=16, precision='', mode='nearest',
         extent:'BBox|str'='', default_opt=DFLT_WARP_OPT, extra_opt='', reuse=False):
     """ Gdalwarp wrapper
         :param z: to reproject/resample to a TMS zoom level `z`
-        :param extent: eg `-te w s e n` in WGS84
+        :param extent: eg `w s e n` in WGS84
         :param default_opt: to override default compression/tiling/tif etc
         :param extra_opt: any additional option"""
-    if reuse and isfile(dest): print('Reuse', dest) ; return 0
+    if reuse and isfile(dest): print('Reuse', dest) ; return dest
     tr = '' if not z else \
          f'-tr {resolutions[z]} -{resolutions[z]}'
     if isinstance(extent, BBox):
-        extent = f'-te {extent}'
+        extent = f'-te_srs WGS84 -te {extent}'
     check_run(printed(f'''\
       gdalwarp {precision} \\
         {default_opt} \\
         -t_srs EPSG:3857 {tr} -r {mode} \\
-        -te_srs WGS84 {extent} {extra_opt} \\
+        {extent} {extra_opt} \\
         {src} {dest}'''))
 
 
-def merge_slopes(*,
+def warp_slopes(*,
         src: str, dest: str, z=16, precision='-ot Byte',
         extent:'BBox|str'='', default_opt=DFLT_WARP_OPT, extra_opt='', reuse=False):
     """Merge/reproject/resample to a TMS zoom level `z`
@@ -113,7 +115,7 @@ def make_ovr(*, src: str, dest='', z,
     tr = resolutions[z]
     # replace the first occurrence of 'z#' or the last file extension with 'z#'
     dest = dest or re.sub(r'(z\d\d?\b)|(\.[^.]+)$', rf'z{z}\2', src, count=1)
-    if reuse and isfile(dest): print('Reuse', dest); return 0
+    if reuse and isfile(dest): print('Reuse', dest); return dest
     check_run(printed(f'''\
       gdalwarp -r {r} -tr {tr} -{tr} \\
         {default_opt} {extra_opt} \\
@@ -133,28 +135,31 @@ def slope_mbt(cname:str, *, z:int, options='', src='', dest='', reuse=False):
         dest = f'{src_foldr}/{src_name.replace(".tif", "")}-{cname}-z{z}.mbtiles'
     else:
         src = src or f'./slopes-z{z}.tif'
-        dest = dest or f'./{cname}-z{z}.mbtiles'
+        dest = dest or f'./{src}-{cname}-z{z}.mbtiles'
     if reuse and isfile(dest): print('Reuse', dest) ; return dest
     cmap = f'gdaldem-slope-{cname}.clr'
     # set nodata value to white so mbtiles blends correctly
-    cmd = rf'''sed 's/nv \+0 \+0 \+0/nv  255 255 255/g' '{CMAPDIR}/{cmap}' '''
+    cmd = rf'''sed -e 's/nv \+0 \+0 \+0/nv  255 255 255/g' -e 's/nv \+#000000/nv #FFFFFF/g' '{CMAPDIR}/{cmap}' '''
     with open(f'/tmp/{cmap}', 'wb') as f:
         f.write(check_output(printed(cmd), shell=True))
+    with open(f'/tmp/{cmap}') as f:
+        for lineno, line in enumerate(f, 1):
+            if '#' in line.split('%')[0]:  # ignore comments after %
+                raise ValueError(f'/tmp/{cmap}:{lineno}: hex color not supported by gdaldem: {line.rstrip()}')
     check_run(printed(rf'''\
       gdaldem color-relief {src} /tmp/{cmap} {dest} \
           -nearest_color_entry -co TILE_FORMAT=png8 {options}'''))
     return dest
 
-def make_overviews(src: str, dest='', reuse=False):
+def slope_mbt_5_zooms(src: str, dest='', reuse=False):
     """Create an mbtile with lower zoom levels, from a z16 slope.
-        Makes overviews with Q3 method as necessary, but prefer making your own.
+        Uses a z12 downsample (q3) as base for z13/z14, upsampled to target zoom.
     """
     zooms = {
-        16: 'eslo13near',
-        15: 'eslo13near',
+        16: 'eslo13cnear',
+        15: 'eslo13cnear',
         14: 'eslo4near',
-        13: 'eslo4near',  # TBD oslo3near
-        # 12: 'oslo2near', # TBD
+        13: 'eslo4near',
     }
     if os.path.exists(dest):
         if reuse:
@@ -162,16 +167,87 @@ def make_overviews(src: str, dest='', reuse=False):
              return dest
         else:
             os.remove(dest)
+    slope_z14 = make_ovr(src=src, z=14, r='q3', reuse=reuse)
+    slope_z12 = make_ovr(src=slope_z14, z=12, r='q3', reuse=reuse)
+    get_slope = {
+        16: src,
+        15: make_ovr(src=slope_z14, dest=slope_z14.replace('.tif', f'_up_z15.tif'),
+                            z=15, r='bilinear', reuse=reuse),
+        14: make_ovr(src=slope_z12, dest=slope_z12.replace('.tif', f'_up_z14.tif'),
+                            z=14, r='bilinear', reuse=reuse),
+        13: make_ovr(src=slope_z12, dest=slope_z12.replace('.tif', f'_up_z13.tif'),
+                            z=13, r='bilinear', reuse=reuse),
+    }
     files=[]
     for i, (z, cname) in enumerate(zooms.items()):
         chkpoint = time()
-        zoomed_slope = src if z==16 else make_ovr(src=src, z=z, reuse=reuse)
-        if zoomed_slope:
-            to_merge = slope_mbt(cname, z=z, src=zoomed_slope, reuse=reuse)
-            files.append(os.path.expanduser(to_merge))  # f'{cname}-z{z}.mbtiles')
+        to_merge = slope_mbt(cname, z=z, src=get_slope[z], reuse=reuse)
+        files.append(os.path.expanduser(to_merge))
         print(f'Step {i+1}/{len(zooms)} completed in {round(time()-chkpoint,1)} seconds')
 
     mbt_merge(*files, dest=dest)
+    add_white_z(dest, z=10)
+    for f in slope_z14, slope_z12, get_slope[15], get_slope[14], get_slope[13]:
+        os.unlink(f)
+    return dest
+
+
+def add_white_z(mbt: str, z=10, border_px=10):
+    """Add a layer of transparent tiles at zoom level `z` covering the real bounds of `mbt`.
+
+    Peripheral tiles get a semi-transparent blue border (alpha 30%) on their outward-facing
+    edges.  Interior tiles are fully transparent white (255,255,255,0).
+    """
+    import io
+    import sqlite3
+    from PIL import Image, ImageDraw
+    from .mbt_util import real_bounds, lnglat2tms, set_real_bounds
+
+    _, _, bb = real_bounds(mbt)
+    _, x_west, y_south = lnglat2tms(z, lng=float(bb.west), lat=float(bb.south))
+    _, x_east, y_north = lnglat2tms(z, lng=float(bb.east), lat=float(bb.north))
+
+    SZ = 256
+    BLUE = (0, 0, 255, 77)  # 30% alpha ≈ 77/255
+
+    def _make_tile(left, right, top, bottom):
+        """Create a 256x256 RGBA PNG. Borders drawn on indicated sides."""
+        img = Image.new('RGBA', (SZ, SZ), (255, 255, 255, 0))
+        if left or right or top or bottom:
+            draw = ImageDraw.Draw(img)
+            b = border_px
+            if top:    draw.rectangle([0, 0, SZ - 1, b - 1], fill=BLUE)
+            if bottom: draw.rectangle([0, SZ - b, SZ - 1, SZ - 1], fill=BLUE)
+            if left:   draw.rectangle([0, 0, b - 1, SZ - 1], fill=BLUE)
+            if right:  draw.rectangle([SZ - b, 0, SZ - 1, SZ - 1], fill=BLUE)
+        buf = io.BytesIO()
+        img.save(buf, format='PNG')
+        return buf.getvalue()
+
+    # Pre-build the transparent interior tile (reused for all interior tiles)
+    interior_png = _make_tile(False, False, False, False)
+
+    db = sqlite3.connect(mbt)
+    dbc = db.cursor()
+    n = 0
+    for x in range(x_west, x_east + 1):
+        for y in range(y_south, y_north + 1):
+            is_left   = (x == x_west)
+            is_right  = (x == x_east)
+            is_bottom = (y == y_south)  # TMS: y_south is the southernmost (lowest) row
+            is_top    = (y == y_north)
+            if is_left or is_right or is_top or is_bottom:
+                tile_png = _make_tile(is_left, is_right, is_top, is_bottom)
+            else:
+                tile_png = interior_png
+            dbc.execute(
+                'INSERT OR REPLACE INTO tiles (zoom_level, tile_column, tile_row, tile_data) VALUES (?, ?, ?, ?)',
+                (z, x, y, tile_png))
+            n += 1
+    set_real_bounds(dbc)
+    db.commit()
+    db.close()
+    print(f'add_white_z: inserted {n} tiles at z={z} ({x_west}..{x_east} x {y_south}..{y_north})')
 
 
 def eslo_tiny(path: str, cname='eslo13bnear', res=0, where='/tmp', reuse=False):
@@ -187,7 +263,7 @@ def eslo_tiny(path: str, cname='eslo13bnear', res=0, where='/tmp', reuse=False):
     if is_slope:
         check_run(f'ln -sf {path} {p_slope}')
     else:
-        cmd = f'gdaldem slope {path} {DFLT_OPT} {p_slope}'
+        cmd = f'gdaldem slope {GDALDEM_CFG} {path} {DFLT_OPT} {p_slope}'
         print(cmd); check_run(cmd)
     cmap = f'{CMAPDIR}/gdaldem-slope-{cname}.clr'
     p_relief = f'{where}/tiny_{cname}.png'
@@ -206,3 +282,12 @@ def relief_tiny(*paths: str, res=0, where='/tmp'):
     p_relief = f'{where}/tiny_relief.png'
     check_run(printed(f'gdaldem color-relief {path} {cmap} {p_relief}'))
     return p_relief
+
+def get_bounds(dataset_path) -> LLBb:
+    """Return WGS84 bounding box of a raster file as LLBb(w, s, e, n)."""
+    import rasterio
+    from rasterio.warp import transform_bounds
+
+    with rasterio.open(dataset_path) as ds:
+        w, s, e, n = transform_bounds(ds.crs, "EPSG:4326", *ds.bounds)
+    return LLBb(w, s, e, n)
